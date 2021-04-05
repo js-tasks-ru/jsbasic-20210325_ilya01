8569197a609814c69d6181e575f3a9ddfc684006
Ilya Kudinov <ilya.kudinov96@yandex.ru>
2021-04-05 17:47:29 +0300

    Вычислить факториал

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,16 @@
 function factorial(n) {
-  // ваш код...
+  let i = 1;
+  let result = 1;
+
+  if ( n === 0 || n === 1 ) {
+    result = 1;
+  } else {
+    while ( i < n ) {
+      result = result * (n - i);
+      i++;
+    }
+    result = result * n;
+  }
+
+  return result;
 }
